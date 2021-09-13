43a9b075887a87a0f7155dc3bc94448585650bd9
Evan Williams <evan.b.williams95@gmail.com>
2021-09-13 15:47:02 -0500

    using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 
@@ -6,11 +6,22 @@ import './App.css';
 
 function App(props) {
     const [emotion, setEmotion] = useState("happy");
+    const [secondary, setSecondary] = useState("tired");
+
+    useEffect(() => {
+        console.log(`It's ${emotion} around here!`);
+    }, [emotion]);
+
+    useEffect(() => {
+        console.log(`It's ${secondary} around here!`);
+    },[secondary])
+
     return(
         <>
-            <h1>Current emotion is {emotion}.</h1>
+            <h1>Current emotion is {emotion} and {secondary}</h1>
             <button onClick={() => setEmotion("Frustrated")}>Frustrate</button>
             <button onClick={() => setEmotion("Enthusiastic")}>Enthuse</button>
+            <button onClick={() => setSecondary("Crabby")}>Make crabby</button>
         </>
         );
 
