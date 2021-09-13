94a90da92da30b95d5941c062c71009aff1c75d9
Evan Williams <evan.b.williams95@gmail.com>
2021-09-13 16:34:19 -0500

    introducing react testing library

--- src/App.js
+++ src/App.js
@@ -4,41 +4,12 @@ import './App.css';
 // https://api.github.com/users/evanwilliamsC137
 
 
-function App( {login} ) {
-
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        if (!login) return;
-        setLoading(true);
-        fetch(`https://api.github.com/users/${login}`)
-            .then(response => response.json())
-            .then(setData)
-            .then(setLoading(false))
-            .catch(setError)
-    },[login]);
-    if (loading) return <h1>loading...</h1>;
-
-    if (error)
-        return <pre>{JSON.stringify(error,null,2)}</pre>;
-    if (!data) return null;
-
-
-    if (data) {
-        return <div>
-            <h1>{data.name}</h1>
-            <p>{data.location}</p>
-            <img alt={data.login} src={data.avatar_url}/>
-        </div>;
-    }
-
+function App() {
     return(
         <div>
-            No user available
+            <h1>Hello React Testing Library</h1>
         </div>
-        );
+    )
 
 }
 
