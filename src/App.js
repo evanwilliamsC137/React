4749a68b744efe739bb152b5b97e776ee060dcd1
Evan Williams <evan.b.williams95@gmail.com>
2021-09-13 16:15:49 -0500

    handling error and loading states

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ import './App.css';
 function App( {login} ) {
 
     const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
+        if (!login) return;
+        setLoading(true);
         fetch(`https://api.github.com/users/${login}`)
             .then(response => response.json())
-            .then(setData);
-    },[]);
+            .then(setData)
+            .then(setLoading(false))
+            .catch(setError)
+    },[login]);
+    if (loading) return <h1>loading...</h1>;
+
+    if (error)
+        return <pre>{JSON.stringify(error,null,2)}</pre>;
+    if (!data) return null;
+
 
     if (data) {
         return <div>
