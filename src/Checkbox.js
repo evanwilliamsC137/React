a39a87563159ea27eb9606531297a99d46340a4f
Evan Williams <evan.b.williams95@gmail.com>
2021-09-16 14:37:05 -0500

    configure router

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,22 +1,22 @@
-import React, {useReducer} from "react";
-
-
-export function Checkbox() {
-    const [checked, toggle] = useReducer(
-        checked => !checked,
-        false
-    );
-
-    return(
-        <>
-            <label htmlFor={"checkbox"}>
-                {checked ? "checked" : "not checked"}
-            </label>
-            <input
-                type={"checkbox"}
-                value={checked}
-                onChange={toggle}
-            />
-        </>
-    )
-}
+// import React, {useReducer} from "react";
+//
+//
+// export function Checkbox() {
+//     const [checked, toggle] = useReducer(
+//         checked => !checked,
+//         false
+//     );
+//
+//     return(
+//         <>
+//             <label htmlFor={"checkbox"}>
+//                 {checked ? "checked" : "not checked"}
+//             </label>
+//             <input
+//                 type={"checkbox"}
+//                 value={checked}
+//                 onChange={toggle}
+//             />
+//         </>
+//     )
+// }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from "react-router-dom";
 
 
 ReactDOM.render(
 
-    <App login={"evanwilliamsC137"}/>,
+    <Router>
+        <App login={"evanwilliamsC137"}/>
+    </Router>
+    ,
   document.getElementById('root')
 );
 reportWebVitals();
