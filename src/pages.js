ce74299fbc6d63fde4a431d965f70b1f51ea359a
Evan Williams <evan.b.williams95@gmail.com>
2021-09-16 14:43:53 -0500

    made use of useLocation hook for error page

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {Link, useLocation} from "react-router-dom";
 
 export function Home() {
     return (
         <div>
             <h1>Company name</h1>
+            <nav>
+                <Link to={"about"}>About</Link>
+                <Link to={"events"}>Events</Link>
+                <Link to={"contact"}>Contact</Link>
+            </nav>
         </div>
     );
 }
@@ -30,4 +36,14 @@ export function Contact() {
             <h1>Contact</h1>
         </div>
     );
+}
+
+export function Whoops404() {
+    let location = useLocation();
+
+    return (
+        <div>
+            <h1>Resource not found at {location.pathname}</h1>
+        </div>
+    )
 }
